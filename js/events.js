484d9f4a20602fccba03c25837b776a334079849
robcol62-GH <robcol62@gmail.com>
2026-09-25 16:05:59 +0200

    Aggiornamento London

--- js/events.js
+++ js/events.js
@@ -4,7 +4,7 @@
 
     static async load() {
 
-        const response = await fetch("data/events.json?v=08041620260925");
+        const response = await fetch("data/events.json?v=58051620260925");
 
         this.events = await response.json();
 
